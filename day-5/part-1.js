294e5ce949acb6360ca5a5198adaa3934fb428b3
Fernanda Cassinelli <fernandafbcassinelli@gmail.com>
2022-12-11 23:18:45 -0300

    Day 5 part 1 refactor

--- day-5/part-1.js
+++ day-5/part-1.js
@@ -1,66 +1,39 @@
 const { input } = require('./input')
 
-const moveCrate = (cratesStacks, topIndexes, origin, destination) => {
-  const crate = cratesStacks[topIndexes[origin - 1]][origin - 1]
-  cratesStacks[topIndexes[origin - 1]][origin - 1] = ''
-  topIndexes[origin - 1] += 1
-  
-  if (topIndexes[destination - 1] === 0) {
-    const newTopLine = []
-    cratesStacks[0].forEach(() => newTopLine.push(''))
-    newTopLine[destination - 1] = crate
-    cratesStacks.unshift(newTopLine)
-    topIndexes[destination - 1] -= 1
-    topIndexes = topIndexes.map(index => index + 1)
-    
-    return [cratesStacks, topIndexes]
-  }
+const moveCrates = (cratesStacks, origin, destination) => {
+  cratesStacks[destination]
+    .push(...cratesStacks[origin].splice(cratesStacks[origin].length - 1, 1))
 
-  cratesStacks[topIndexes[destination - 1] - 1][destination - 1] = crate
-  topIndexes[destination - 1] -= 1
+  return cratesStacks
 
-  if (cratesStacks[0].every(val => val === '')) {
-    cratesStacks.shift()
-    topIndexes = topIndexes.map(index => index - 1)
-  }
-  return [cratesStacks, topIndexes]
-} 
-
-const getTopIndexes = (cratesStacks) => {
-  const topIndexes = []
-  cratesStacks[0].forEach(() => topIndexes.push(''))
-  for (let i = 0; i < cratesStacks.length; i += 1) {
-    for (let j = 0; j < cratesStacks[0].length; j += 1) {
-      if (cratesStacks[i][j] !== '' && topIndexes[j] === '') {
-        topIndexes[j] = i
-      }
-    }
-  }
-  return topIndexes
 }
 
-const [stacks, commands] = input.replaceAll('    ', ' ').split('\n\n')
+const [stacksInput, commands] = input.replaceAll('    ', ' ').split('\n\n')
 
-const lines = stacks.split('\n')
+const lines = stacksInput.split('\n')
 lines.pop()
 
 let crates = lines.map(line => line.split(' '))
-let stacksTopIndexes = getTopIndexes(crates)
+let stacks = crates[0].map(() => [])
+
+for (let i = crates.length - 1 ; i >= 0 ; i -= 1) {
+  for (let j = 0 ; j < crates[0].length ; j += 1) {
+    const crate = crates[i][j]
+    if (crate !== '') {
+      stacks[j].push(crate[1])
+    }
+  }
+}
 
 commands
   .split('\n')
   .map(command => command.split(' '))
   .map(command => {
     for (let i = 1; i <= +command[1]; i += 1) {
-      [crates, stacksTopIndexes] = moveCrate(crates, stacksTopIndexes, +command[3], +command[5])
+      stacks = moveCrates(stacks, +command[3] - 1, +command[5] - 1)
     }
   })
 
-let index = 0
-const topCrates = stacksTopIndexes.map(topIndex => {
-  const crate = crates[topIndex][index]
-  index += 1
-  return crate[1]
-})
+const topCrates = stacks.map(stack => stack[stack.length - 1]).join('')
 
-console.log(topCrates.join(''))
+console.log(topCrates)
