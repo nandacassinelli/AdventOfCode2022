091267d0e8b0b301c76fe9e65303f358cabf5fb9
Fernanda Cassinelli <fernandafbcassinelli@gmail.com>
2022-12-11 23:18:54 -0300

    Day 5 part 2

---
+++ day-5/part-2.js
@@ -0,0 +1,38 @@
+const { input } = require('./input')
+
+const moveCrates = (cratesStacks, origin, destination, quantity) => {
+  cratesStacks[destination]
+    .push(...cratesStacks[origin].splice(cratesStacks[origin].length - quantity, quantity))
+
+  return cratesStacks
+
+}
+
+const [stacksInput, commands] = input.replaceAll('    ', ' ').split('\n\n')
+
+const lines = stacksInput.split('\n')
+lines.pop()
+
+let crates = lines.map(line => line.split(' '))
+
+let stacks = crates[0].map(() => [])
+
+for (let i = crates.length - 1 ; i >= 0 ; i -= 1) {
+  for (let j = 0 ; j < crates[0].length ; j += 1) {
+    const crate = crates[i][j]
+    if (crate !== '') {
+      stacks[j].push(crate[1])
+    }
+  }
+}
+
+commands
+  .split('\n')
+  .map(command => command.split(' '))
+  .map(command => {
+    stacks = moveCrates(stacks, +command[3] - 1, +command[5] - 1, +command[1])
+  })
+
+const topCrates = stacks.map(stack => stack[stack.length - 1]).join('')
+
+console.log(topCrates)
